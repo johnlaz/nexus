963eaef52e8c6eeb7dfc68890ea5c220e766a283
johnlaz <51280623+johnlaz@users.noreply.github.com>
2026-04-26 12:03:37 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// NEXUS v2.0 — Service Worker
-const CACHE_NAME = 'nexus-v2.5';
+// NEXUS v4.0 — Service Worker
+const CACHE_NAME = 'nexus-v4.0';
 const STATIC_ASSETS = [
   './index.html',
   './manifest.json',
